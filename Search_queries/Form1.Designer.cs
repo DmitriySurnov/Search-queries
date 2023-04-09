b6b03a4c1bf12f5e3695b02c1bdbb3ab83f54d0d
DmitriySurnov <79646813832@yandex.ru>
2023-04-09 19:34:26 +0300

    Completing a task

---
+++ Search_queries/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Poisk
+{
+    partial class FormInformationSearch
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.labelSearchEngine = new System.Windows.Forms.Label();
+            this.textBoxQueryString = new System.Windows.Forms.TextBox();
+            this.labelQueryString = new System.Windows.Forms.Label();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.labelNameSearchEngine = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(933, 13);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(232, 105);
+            this.buttonSearch.TabIndex = 0;
+            this.buttonSearch.Text = "Найти";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.ButtonSearch_Click);
+            // 
+            // labelSearchEngine
+            // 
+            this.labelSearchEngine.Location = new System.Drawing.Point(13, 13);
+            this.labelSearchEngine.Name = "labelSearchEngine";
+            this.labelSearchEngine.Size = new System.Drawing.Size(307, 46);
+            this.labelSearchEngine.TabIndex = 2;
+            this.labelSearchEngine.Text = "Поисковая система";
+            // 
+            // textBoxQueryString
+            // 
+            this.textBoxQueryString.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxQueryString.Location = new System.Drawing.Point(326, 62);
+            this.textBoxQueryString.Name = "textBoxQueryString";
+            this.textBoxQueryString.Size = new System.Drawing.Size(573, 56);
+            this.textBoxQueryString.TabIndex = 4;
+            // 
+            // labelQueryString
+            // 
+            this.labelQueryString.Location = new System.Drawing.Point(346, 13);
+            this.labelQueryString.Name = "labelQueryString";
+            this.labelQueryString.Size = new System.Drawing.Size(514, 25);
+            this.labelQueryString.TabIndex = 5;
+            this.labelQueryString.Text = "Введите запросс для поиска";
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 25;
+            this.listBoxResult.Location = new System.Drawing.Point(18, 133);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(1618, 404);
+            this.listBoxResult.Sorted = true;
+            this.listBoxResult.TabIndex = 6;
+            // 
+            // labelNameSearchEngine
+            // 
+            this.labelNameSearchEngine.Location = new System.Drawing.Point(12, 59);
+            this.labelNameSearchEngine.Name = "labelNameSearchEngine";
+            this.labelNameSearchEngine.Size = new System.Drawing.Size(282, 46);
+            this.labelNameSearchEngine.TabIndex = 7;
+            this.labelNameSearchEngine.Text = "гугл";
+            this.labelNameSearchEngine.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormInformationSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1651, 551);
+            this.Controls.Add(this.labelNameSearchEngine);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.labelQueryString);
+            this.Controls.Add(this.textBoxQueryString);
+            this.Controls.Add(this.labelSearchEngine);
+            this.Controls.Add(this.buttonSearch);
+            this.Name = "FormInformationSearch";
+            this.Text = "Поиск инфации";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Label labelSearchEngine;
+        private System.Windows.Forms.TextBox textBoxQueryString;
+        private System.Windows.Forms.Label labelQueryString;
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.Label labelNameSearchEngine;
+    }
+}
+
---
+++ Search_queries/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Poisk
+{
+    public partial class FormInformationSearch : System.Windows.Forms.Form
+    {
+        private int _position = 0;
+
+        public FormInformationSearch()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            listBoxResult.Items.Clear();
+            if (textBoxQueryString.Text == "")
+                return;
+            Uri baseURI = new Uri("https://www.google.com");
+            Uri newURI = new Uri(baseURI, "search?q=" + textBoxQueryString.Text.Replace(" ", "+"));
+            HttpWebRequest reqw = (HttpWebRequest)HttpWebRequest.Create(newURI);
+            reqw.UserAgent = "SimpleHostClient";
+            HttpWebResponse resp = (HttpWebResponse)reqw.GetResponse(); //создаем объект отклика
+            StreamReader sr = new StreamReader(resp.GetResponseStream(), Encoding.Default);
+            //создаем поток для чтения отклика
+            string htmlDocument = sr.ReadToEnd();
+            for (int i = 0; i < 10; i++)
+            {
+                string resultingString = ParsingHTML(htmlDocument, "<h3", "</div>", '>');
+                if (resultingString == "")
+                    continue;
+                listBoxResult.Items.Add(ParsingHTML(resultingString));
+            }
+            sr.Close();
+        }
+
+        private string ParsingHTML(string htmlDocument, string beginningOfLine, 
+            string endOfLine, char symbolOfBeginningOfSubstring)
+        {
+            int strtIndex = htmlDocument.IndexOf(beginningOfLine, _position);
+            if (strtIndex == -1)
+                return "";
+            int endIndex = htmlDocument.IndexOf(endOfLine, strtIndex);
+            if (endIndex == -1) 
+                return "";
+            string resultingString = htmlDocument.Substring(strtIndex, endIndex - strtIndex);
+            resultingString = resultingString.Remove(0, beginningOfLine.Length);
+            _position = endIndex;
+            resultingString = resultingString.Replace(";", "");
+            resultingString = resultingString.Remove(0, resultingString.LastIndexOf(symbolOfBeginningOfSubstring) + 1);
+            return resultingString;
+        }
+
+        private string ParsingHTML(string htmlDocument)
+        {
+            int strtIndex = htmlDocument.IndexOf("&#");
+            if (strtIndex == -1)
+                return htmlDocument;
+            string Code = htmlDocument.Substring(strtIndex, 6);
+            Code = Code.Replace("&#", "");
+            int сharacterCode = int.Parse(Code);
+            char symbol = (char)сharacterCode;
+            Code = htmlDocument.Substring(strtIndex, 6);
+            htmlDocument = htmlDocument.Replace(Code, symbol.ToString());
+            return ParsingHTML(htmlDocument);
+        }
+    }
+}
